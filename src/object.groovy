8051c458c97e9f51eacd5a02be7f33208f2b5f97
hatlonely <hatlonely@gmail.com>
2018-03-21 14:56:11 +0800

    update

---
+++ src/object.groovy
@@ -0,0 +1,34 @@
+class Person {
+
+    String name
+    Integer age
+
+    Person(String name, Integer age) {
+        this.name = name
+        this.age = age
+    }
+
+    def increaseAge(Integer years) {
+        this.age += years
+    }
+}
+
+person = new Person("hatlonely", 26)
+person.age = 26
+person.increaseAge(4)
+println(person.name + " "+ person.age)
+
+interface Named {
+    String name()
+}
+trait Greetable implements Named {
+    String greeting() { "Hello, ${name()}!" }
+}
+class Person2 implements Greetable {
+    String name() { 'Bob' }
+}
+
+def p = new Person2()
+assert p.greeting() == 'Hello, Bob!'
+assert p instanceof Named
+assert p instanceof Greetable
